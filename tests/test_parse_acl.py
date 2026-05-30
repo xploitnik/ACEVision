e8eee905e973a9f7633fdac0a4da87b5e2784ac3
xploiNik <82586952+xploitnik@users.noreply.github.com>
2026-05-29 20:28:45 -0400

    Delete tests directory

--- tests/test_parse_acl.py
+++
@@ -1,8 +0,0 @@
-from certipy_tool.parse_acl import summarize_mask
-
-def test_genericall_equivalent():
-    # máscara de tu caso real (full control expandido sin bit GA directo)
-    mask = 0x0F01FF
-    kk = summarize_mask(mask)
-    assert kk["GenericAll_derived"] is True
-    assert kk["GenericAll_direct"] is False
